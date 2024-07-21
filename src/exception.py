c0d47852728f49ba5d8cb849fc1084f603406905
LovePlayingChess <ganeshvenu705@gmail.com>
2024-07-21 15:48:22 -0700

    Data Injestion

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,8 @@
 import sys
 import logging
-from src.logger import logging
+from .logger import logging
 
 def error_message_details(error, error_detail: sys):
-
     # need filename, line number, string_error_message
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -22,15 +21,11 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-
-#'''
-# For Testing Purposes
 if __name__ == "__main__":
     try:
         a = 1/0 # raise a division by zero exception
     except Exception as e:
         logging.info("Error: Division by Zero")
         raise CustomException(e, sys)
-#'''
         
 
